dd5f8ebb90147110e896fccae004a69e760f9838
Nataly Tuong <ompwomp@nataly.local>
2025-12-28 21:08:26 -0800

    rate limit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -9,13 +10,17 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(rateLimiter);
 
-connectDB();
-
-app.use(express.json());
+// app.use(express.json());
+// app.use((req, res, next) => {
+//   console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
-
-app.listen(PORT, () => {
-  console.log("Server started on PORT", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
