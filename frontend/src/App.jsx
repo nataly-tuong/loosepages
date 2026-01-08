0f0ae5e7a943a2517c499b5a138b8ee4ffa2c5a6
nataly-tuong <152276169+nataly-tuong@users.noreply.github.com>
2026-01-08 01:22:05 -0800

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import NoteDetailPage from "./pages/NoteDetailPage.jsx";
+import toast from "react-hot-toast";
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => toast.success("congrats")}>Click Me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
