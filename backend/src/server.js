7ca07478d42a63f8027e1ce4ff4cd5db0aa049cb
nataly-tuong <152276169+nataly-tuong@users.noreply.github.com>
2026-01-08 02:41:25 -0800

    rate limit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
@@ -9,14 +11,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
-
-// app.use(express.json());
-// app.use((req, res, next) => {
-//   console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
-//   next();
-// });
 
 app.use("/api/notes", notesRoutes);
 connectDB().then(() => {
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
-  const [isRateLimited, setRateLimited] = useState(true);
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log(error);
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
 
   return (
     <div className="bg-[#090909] min-h-screen">
