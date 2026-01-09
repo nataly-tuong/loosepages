098f7d052b4a7eb199cf7b68bddec5ee25ead095
Nataly Tuong <152276169+nataly-tuong@users.noreply.github.com>
2026-01-08 20:02:35 -0800

    create page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const CreatePage = () => {
-  return <div>CreatePage</div>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {
+
+  }
+
+  return <div className="min-h-screen bg-base-200">
+    <div className="container mx-auto px-4 py-8">
+      
+    </div>
+  </div>;
 };
 
 export default CreatePage;
